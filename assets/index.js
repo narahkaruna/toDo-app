49a1876903550b6901c46f29f41475b6e1baab55
narahkaruna <narah.karuna@gmail.com>
2022-09-22 15:54:29 +1000

    create label and checkbox in js

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
-let todoList = [];
+let todoList = ['laughing', 'shopping', 'cleaning'];
 
-let handleTodos = {
+let todos = {
     displayTodo: todoList,
     addTodo: (todo) => {
         todoList.push(todo)
@@ -15,5 +15,33 @@ let handleTodos = {
     }
 };
 
+let taskList = document.querySelector('#taskList');
+let addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addTask)
 
+function addTask(){
+    let inputTask = document.querySelector('#task').value;
+    todos.addTodo(inputTask);
+}
+
+function displayTask(){
+    for(let i = 0; i < todoList.length; i++){
+        let id = `task${i+1}`;
+        //Create label
+        let label = document.createElement('label');
+        label.htmlFor = id;
+
+        //Create checkbox
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = id;
+        checkbox.name = 'task';
+        checkbox.innerHTML = todoList[i];
+
+        taskList.appendChild(label);
+        taskList.appendChild(checkbox);
+    }
+    
+}
+displayTask();
 
